0d29e48606779bacbf67f44c52b7792512c7e303
sathvika855 <lellasathvika18@gmail.com>
2024-12-06 16:17:43 +0530

    App.java

--- feeds/src/main/java/facebook/App.java
+++ feeds/src/main/java/facebook/App.java
@@ -5,6 +5,6 @@ package facebook;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!i am sathvika");
+        System.out.println("Hello World!i am sathvika,sathi");
     }
 }
